9fec385b9084b217815715cd76cf5b01670ed2a6
woods <Spencer.Wood@webtrends.com>
2016-10-04 19:53:48 -0700

    Cleanup of old dirs

--- spenk-core/src/main/scala/com/wood/spenk/query/Selector.scala
+++
@@ -1,9 +0,0 @@
-package com.wood.spenk.query
-
-import com.wood.spenk.directory.Fields.Field
-
-case class Selector(field: Field, agg: Option[Aggregate]) {
-  def select(line: Array[String]): String = {
-    line(field.index)
-  }
-}
